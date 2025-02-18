fbeb232b521a51d44d9f1847516f8dbf50673b83
mihai271828 <pruneanumihai@gmail.com>
2025-02-18 11:26:51 +0200

    InMemoryRepo

--- src/Repository/InMemoryRepository.java
+++ src/Repository/InMemoryRepository.java
@@ -1,4 +1,56 @@
 package Repository;
 
-public class InMemoryRepository {
+import Model.HasId;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A repository implementation that stores data in memory.
+ *
+ * @param <T> The type of objects stored in the repository, which must implement HasId.
+ */
+public class InMemoryRepository <T extends HasId> implements Repository<T> {
+    private final Map<Integer, T> data = new HashMap<>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void create(T obj) {
+        data.putIfAbsent(obj.getId(), obj);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public T get(Integer id) {
+        return data.get(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update(T obj) {
+        data.replace(obj.getId(), obj);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(Integer id) {
+        data.remove(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<T> getAll() {
+        return data.values().stream().toList();
+    }
 }
